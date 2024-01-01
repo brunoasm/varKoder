1fc1da6003c2cb470a8ffa70907c0f8d6dd08763
bdemedeiros <bdemedeiros@fieldmuseum.org>
2023-12-31 20:03:52 -0600

    reformatted python code

--- setup.py
+++ setup.py
@@ -1,28 +1,27 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='varKoder',
-    version='0.9.0',  # Update based on your versioning scheme
+    name="varKoder",
+    version="0.9.0",
     packages=find_packages(),
-    url='https://github.com/brunoasm/varKoder',
-    license='GNU General Public License',  # Specify GPL version if applicable
-    author='Bruno de Medeiros',  # Replace with your name
-    author_email='bdemedeiros@fieldmuseum.org',  # Optionally, replace with your email
-    description='A tool that uses variation in K-mer frequencies as DNA barcodes.',
+    url="https://github.com/brunoasm/varKoder",
+    license="GNU General Public License",
+    author="Bruno de Medeiros", 
+    author_email="bdemedeiros@fieldmuseum.org", 
+    description="A tool that uses variation in K-mer frequencies as DNA barcodes.",
     install_requires=[
-        'torch',
-        'fastai>=2.7.12',
-        'timm==0.6.13',
-        'pyarrow',
-        'pandas',
-        'humanfriendly',
-        'tenacity'
+        "torch",
+        "fastai>=2.7.12",
+        "timm==0.6.13",
+        "pyarrow",
+        "pandas",
+        "humanfriendly",
+        "tenacity",
     ],
     include_package_data=True,
     entry_points={
-        'console_scripts': [
-            'varKoder=varKoder.varKoder:main'  # Adjust if using a different function name
+        "console_scripts": [
+            "varKoder=varKoder.varKoder:main"  
         ]
-    }
+    },
 )
-
