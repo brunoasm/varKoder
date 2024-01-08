71ec49dc76117368a08c99d6b7015816307405d8
Bruno de Medeiros <bdemedeiros@fieldmuseum.org>
2024-01-07 20:52:39 -0600

    Simplified query df generation

--- varKoder/varKoder.py
+++ varKoder/varKoder.py
@@ -543,6 +543,27 @@ def main():
         df = pd.DataFrame({"path": img_paths})
         query_dl = learn.dls.test_dl(df, bs=args.max_batch_size)
 
+        # Predicting and construction output dataframe
+        common_data = {
+            "varKode_image_path": img_paths,
+            "sample_id": [
+                img.with_suffix("").name.split(sample_bp_sep)[0].split(label_sample_sep)[-1]
+                for img in img_paths
+            ],
+            "query_basepairs": [
+                img.with_suffix("").name.split(sample_bp_sep)[-1].split(bp_kmer_sep)[0]
+                for img in img_paths
+            ],
+            "query_kmer_len": [
+                img.with_suffix("").name.split(sample_bp_sep)[-1].split(bp_kmer_sep)[-1]
+                for img in img_paths
+            ],
+            "trained_model_path": str(args.model),
+            "actual_labels": actual_labels,
+            "possible_low_quality": qual_flag,
+            "basefrequency_sd": freq_sd,
+        }
+        
         if "MultiLabel" in str(learn.loss_func):
             eprint(
                 "This is a multilabel classification model, each input may have 0 or more predictions."
@@ -551,113 +572,36 @@ def main():
             above_threshold = pp >= args.threshold
             vocab = learn.dls.vocab
             predicted_labels = [
-                 ";".join([vocab[idx] for idx, val in enumerate(row) if val])
-                 for row in above_threshold
-             ]
-
-
-
-            output_df = pd.DataFrame(
-                        {
-                            "varKode_image_path": img_paths,
-                            "sample_id": [
-                                (
-                                    img.with_suffix("")
-                                    .name.split(sample_bp_sep)[0]
-                                    .split(label_sample_sep)[-1]
-                                )
-                                for img in img_paths
-                            ],
-                            "query_basepairs": [
-                                (
-                                    img.with_suffix("")
-                                    .name.split(sample_bp_sep)[-1]
-                                    .split(bp_kmer_sep)[0]
-                                )
-                                for img in img_paths
-                            ],
-                            "query_kmer_len": [
-                                (
-                                    img.with_suffix("")
-                                    .name.split(sample_bp_sep)[-1]
-                                    .split(bp_kmer_sep)[-1]
-                                )
-                                for img in img_paths
-                            ],
-                            "trained_model_path": str(args.model),
-                            "prediction_type": "Multilabel",
-                            "prediction_threshold": args.threshold,
-                            "predicted_labels": predicted_labels,
-                            "actual_labels": actual_labels,
-                            "possible_low_quality": qual_flag,
-                            "basefrequency_sd": freq_sd,
-                        }
-                    )
-
-            if args.include_probs:
-                output_df = pd.concat([
-                        output_df,
-                        pd.DataFrame(pp),
-                    ],
-                    axis=1
-                )
+                ";".join([vocab[idx] for idx, val in enumerate(row) if val])
+                for row in above_threshold
+            ]
+        
+            output_df = pd.DataFrame({
+                **common_data,
+                "prediction_type": "Multilabel",
+                "prediction_threshold": args.threshold,
+                "predicted_labels": predicted_labels,
+            })
+        
         else:
             eprint(
                 "This is a single label classification model, each input may will have only one prediction."
             )
             pp, _ = learn.get_preds(dl=query_dl)
-            predictions_df = pd.DataFrame(pp)
-            predictions_df.columns = learn.dls.vocab
-
+        
             best_ps, best_idx = torch.max(pp, dim=1)
             best_labels = learn.dls.vocab[best_idx]
+        
+            output_df = pd.DataFrame({
+                **common_data,
+                "prediction_type": "Single label",
+                "best_pred_label": best_labels,
+                "best_pred_prob": best_ps,
+            })
+        
+        if args.include_probs:
+            output_df = pd.concat([output_df, pd.DataFrame(pp, columns=learn.dls.vocab)], axis=1)
 
-
-            output_df = pd.DataFrame(
-                        {
-                            "varKode_image_path": img_paths,
-                            "sample_id": [
-                                (
-                                    img.with_suffix("")
-                                    .name.split(sample_bp_sep)[0]
-                                    .split(label_sample_sep)[-1]
-                                )
-                                for img in img_paths
-                            ],
-                            "query_basepairs": [
-                                (
-                                    img.with_suffix("")
-                                    .name.split(sample_bp_sep)[-1]
-                                    .split(bp_kmer_sep)[0]
-                                )
-                                for img in img_paths
-                            ],
-                            "query_kmer_len": [
-                                (
-                                    img.with_suffix("")
-                                    .name.split(sample_bp_sep)[-1]
-                                    .split(bp_kmer_sep)[-1]
-                                )
-                                for img in img_paths
-                            ],
-                            "trained_model_path": str(args.model),
-                            "prediction_type": "Single label",
-                            "best_pred_label": best_labels,
-                            "actual_labels": actual_labels,
-                            "possible_low_quality": qual_flag,
-                            "basefrequency_sd": freq_sd,
-                            "best_pred_prob": best_ps,
-                        }
-                    )
-
-            if args.include_probs:
-                output_df = pd.concat(
-                    [ 
-                        output_df,
-                        pd.DataFrame(pp),
-                    ],
-                    axis=1
-                )
         outdir = Path(args.outdir)
         outdir.mkdir(parents=True, exist_ok=True)
         output_df.to_csv(outdir / "predictions.csv")
