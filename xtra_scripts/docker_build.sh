90e97a522310b0ab56522f73cc83eff816da98d2
Bruno de Medeiros <bdemedeiros@fieldmuseum.org>
2024-01-13 11:54:09 -0600

    add tests to .gitignore

--- xtra_scripts/docker_build.sh
+++ xtra_scripts/docker_build.sh
@@ -1,2 +1,2 @@
-#building focker image on a Mac
+#building docker image on a Mac
 docker buildx build --load --platform linux/amd64 --cache-from type=local,src=/tmp/buildx-cache --cache-to type=local,dest=/tmp/buildx-cache -t brunoasm/varkoder:latest .
