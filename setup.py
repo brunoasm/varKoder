940fa8176f17028661b74dae4d3d4c78b9fca518
bdemedeiros <bdemedeiros@fieldmuseum.org>
2024-05-22 16:36:41 -0500

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="varKoder",
-    version="0.16.2",
+    version="1.0.0a",
     packages=find_packages(),
     url="https://github.com/brunoasm/varKoder",
     license="GNU General Public License",
